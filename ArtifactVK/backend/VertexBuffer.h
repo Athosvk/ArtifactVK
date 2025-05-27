515750b6aa30c57a826164373540dfbfbe33ac6a
Athos van Kralingen <Athosvk@users.noreply.github.com>
2025-05-27 18:09:05 +0200

    Allocate vertex buffer

--- ArtifactVK/backend/VertexBuffer.h
+++ ArtifactVK/backend/VertexBuffer.h
@@ -4,11 +4,13 @@
 #include <vector>
 #include <stdexcept>
 
+#include "VulkanDevice.h"
+
 template<typename T>
 class VertexBuffer
 {
   public:
-    VertexBuffer(std::vector<T> data, VkDevice device, const VkPhysicalDevice& physicalDevice)
+    VertexBuffer(std::vector<T> data, VkDevice device, const VulkanDevice& physicalDevice)
     {
         VkBufferCreateInfo bufferCreateInfo{};
 		bufferCreateInfo.sType = VkStructureType::VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
@@ -25,7 +27,18 @@ class VertexBuffer
 
         VkMemoryRequirements memoryRequirements;
         vkGetBufferMemoryRequirements(device, m_Buffer, &memoryRequirements);
+        auto typeIndex = physicalDevice.FindMemoryType(memoryRequirements.memoryTypeBits,
+                                                   VkMemoryPropertyFlagBits::VK_MEMORY_PROPERTY_HOST_COHERENT_BIT |
+                                                       VkMemoryPropertyFlagBits::VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT);
+        VkMemoryAllocateInfo allocationInfo;
+        allocationInfo.sType = VkStructureType::VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+        allocationInfo.memoryTypeIndex = typeIndex;
+        allocationInfo.allocationSize = memoryRequirements.size;
 
+        if (vkAllocateMemory(device, allocationInfo, nullptr, m_Memory))
+        {
+            throw std::runtime_error("Could not llocate vertex buffer");
+        }
     }
 
     VertexBuffer(const VertexBuffer &) = delete;
@@ -36,10 +49,17 @@ class VertexBuffer
     ~VertexBuffer()
     {
         vkDestroyBuffer(m_Device, m_Buffer, nullptr);
+        vkFreeMemory(m_Device, m_Buffer, nullptr);
+    }
+
+    void Bind(uint32_t slot)
+    {
+        vkBindBufferMemory(m_Device, m_Buffer, m_Memory, 0);
     }
 
 
 private:
     VkBuffer m_Buffer;
+    VkDeviceMemory m_Memory;
     VkDevice m_Device;
 };
