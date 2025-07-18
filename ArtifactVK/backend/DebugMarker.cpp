7c07350794cf28f70090f9f9dac9e50926b966d2
Athos van Kralingen <Athosvk@users.noreply.github.com>
2025-07-19 01:29:26 +0200

    Add other options

--- ArtifactVK/backend/DebugMarker.cpp
+++ ArtifactVK/backend/DebugMarker.cpp
@@ -1,22 +1,43 @@
 #include "DebugMarker.h"
 
 #include <string>
+#include <stdexcept>
 
 void DebugMarker::SetName(VkDevice device, const ExtensionFunctionMapping &extensionMapper, VkCommandBuffer handle,
                           const std::string &name)
 {
-    auto setNameFn = (PFN_vkSetDebugUtilsObjectNameEXT)extensionMapper.GetFunction(EExtensionFunction::DebugUtilsSetObjectName);
-
-    auto info = GetDebugObjectName(name);
-    info.objectType = VkObjectType::VK_OBJECT_TYPE_COMMAND_BUFFER;
-    info.objectHandle = reinterpret_cast<uint64_t>(handle);
-    setNameFn(device, &info);
+    SetNameInternal(name, extensionMapper, device, reinterpret_cast<uint64_t>(handle), VkObjectType::VK_OBJECT_TYPE_COMMAND_BUFFER);
 }
 
-VkDebugUtilsObjectNameInfoEXT DebugMarker::GetDebugObjectName(const std::string& name)
+void DebugMarker::SetName(VkDevice device, const ExtensionFunctionMapping &extensionMapper, VkDescriptorSet handle,
+                          const std::string &name)
 {
-    return VkDebugUtilsObjectNameInfoEXT{.sType = VkStructureType::VK_STRUCTURE_TYPE_DEBUG_MARKER_OBJECT_NAME_INFO_EXT,
+
+    SetNameInternal(name, extensionMapper, device, reinterpret_cast<uint64_t>(handle), VkObjectType::VK_OBJECT_TYPE_DESCRIPTOR_SET);
+    
+}
+
+void DebugMarker::SetName(VkDevice device, const ExtensionFunctionMapping &extensionMapper, VkCommandPool handle,
+                          const std::string &name)
+{
+    SetNameInternal(name, extensionMapper, device, reinterpret_cast<uint64_t>(handle), VkObjectType::VK_OBJECT_TYPE_COMMAND_POOL);
+}
+
+void DebugMarker::SetNameInternal(const std::string& name, const ExtensionFunctionMapping& mapping, VkDevice device,
+    uint64_t handle, VkObjectType handleType)
+{
+    auto setNameFn = reinterpret_cast<PFN_vkSetDebugUtilsObjectNameEXT>(
+        mapping.GetFunction(EExtensionFunction::DebugUtilsSetObjectName));
+    auto info = VkDebugUtilsObjectNameInfoEXT{.sType = VkStructureType::VK_STRUCTURE_TYPE_DEBUG_UTILS_OBJECT_NAME_INFO_EXT,
                                           .pNext = nullptr,
-                                          .pObjectName = name.c_str()};
+                                         .objectType = handleType,
+                                         .objectHandle = handle,
+                                         .pObjectName = name.c_str()
+    };
+
+    if (setNameFn(device, &info) != VkResult::VK_SUCCESS)
+    {
+        throw std::runtime_error("Could not set name");
+    }
 }
 
--- ArtifactVK/backend/DebugMarker.h
+++ ArtifactVK/backend/DebugMarker.h
@@ -10,7 +10,12 @@ class DebugMarker
   public:
     static void SetName(VkDevice vkDevice, const ExtensionFunctionMapping &extensionMapper, VkCommandBuffer handle, 
         const std::string& name);
+    static void SetName(VkDevice vkDevice, const ExtensionFunctionMapping &extensionMapper, VkDescriptorSet handle, 
+        const std::string& name);
+    static void SetName(VkDevice vkDevice, const ExtensionFunctionMapping &extensionMapper, VkCommandPool handle, 
+        const std::string& name);
 
   private:
-    static VkDebugUtilsObjectNameInfoEXT GetDebugObjectName(const std::string& name);
+    static void SetNameInternal(const std::string& name, const ExtensionFunctionMapping& mapping, VkDevice device,
+        uint64_t handle, VkObjectType handleType);
 };
